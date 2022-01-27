76a2671152649063afff1d6aa38b8050a537cc5f
MekonenWase <mekonen.wase@gmail.com>
2022-01-27 19:54:45 +0200

    Tickets components

--- src/components/FlightDetails.jsx
+++ src/components/FlightDetails.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import styles from "../Screens/Flights-Results/FlightsResult.module.css";
+import Tickets from "./Tickets";
 
 function FlightDetails({ search, foundTickets }) {
   return (
@@ -28,43 +29,7 @@ function FlightDetails({ search, foundTickets }) {
           })
         : ""}
       <div className={styles.results}>
-        {foundTickets
-          ? foundTickets.map((ticket) => {
-              return (
-                <div className={styles.ticket}>
-                  <section className={styles.flightDetails}>
-                    <div className="flight num">
-                      <h4>{ticket.flightNumber}</h4>
-                    </div>
-                    <div className="depArvTime">
-                      <h4>
-                        {ticket.departure} - {ticket.arrival}
-                      </h4>
-                    </div>
-                    <div className="fromTo">
-                      <h4>{ticket.origin}</h4>
-                      <h4>{ticket.destination}</h4>
-                    </div>
-                    <div className="total">
-                      <h4>{ticket.terminal}</h4>
-                      <h4>06h 00m</h4>
-                    </div>
-                  </section>
-                  <section className={styles.flightPrice}>
-                    <div className="icon">
-                      <span>icon</span>
-                    </div>
-                    <div className="price">
-                      <span>400$</span>
-                    </div>
-                    <div className="button">
-                      <button>Book</button>
-                    </div>
-                  </section>
-                </div>
-              );
-            })
-          : null}
+        <Tickets foundTickets={foundTickets} />
       </div>
     </div>
   );
