b560d20155a6c376e40cacec96137e0b5eb7da0f
MekonenWase <mekonen.wase@gmail.com>
2022-02-10 00:22:24 +0200

    validation travel detail

--- src/components/BookingDetails/BookingTravelDetails/BookingTravelDetails.jsx
+++ src/components/BookingDetails/BookingTravelDetails/BookingTravelDetails.jsx
@@ -1,36 +1,73 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "./BookingTravelDetails.module.css";
 function BookingTravelDetails() {
+  const [userDetails, setUserDetails] = useState({
+    email: "",
+    firstName: "",
+    lastName: "",
+    dateOfBirth: "",
+    gender: "",
+  });
+
   return (
     <>
       <form className={styles.form}>
         <section className={styles.emailInput}>
-          <input type="email" placeholder="Email" required />
+          <input
+            type="email"
+            placeholder="Email"
+            required
+            onChange={(e) => {
+              setUserDetails({ ...userDetails, email: e.target.value });
+            }}
+          />
         </section>
         <section className={styles.travelDetailInputs}>
           <input
             className={styles.nameInput}
             type="text"
             placeholder="First Name"
+            onChange={(e) => {
+              setUserDetails({ ...userDetails, firstName: e.target.value });
+            }}
           />
           <input
             className={styles.lastNameInput}
             type="text"
             placeholder="Last Name"
+            onChange={(e) => {
+              setUserDetails({ ...userDetails, lastName: e.target.value });
+            }}
           />
-          <input
-            className={styles.birthDayInput}
-            type="text"
-            placeholder=" Date Of Birth"
-          />
-          <select className={styles.selectOption}>
-            <option value={"select"}>select</option>
-            <option value={"Male"}>Male</option>
-            <option value={"Female"}>Female</option>
+          <div className={styles.birthDayInput}>
+            <input
+              className={styles.year}
+              type="date"
+              min="1900-01-01"
+              max="today"
+              onChange={(e) => {
+                setUserDetails({ ...userDetails, dateOfBirth: e.target.value });
+              }}
+            />
+          </div>
+          <select
+            className={styles.selectOption}
+            value={userDetails.gender}
+            onChange={(e) => {
+              setUserDetails({ ...userDetails, gender: e.target.value });
+            }}
+          >
+            <option></option>
+            <option>Male</option>
+            <option>Female</option>
           </select>
         </section>
         <section className={styles.continue}>
-          <button className={styles.continueBtn}> Continue</button>
+          <input
+            type="submit"
+            className={styles.continueBtn}
+            value="Continue"
+          />
         </section>
       </form>
     </>
